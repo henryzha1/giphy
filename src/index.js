618b9a640ecdc52a870a63f7f409231126b8d2d4
Henry Zhang <henz51111@gmail.com>
2024-02-06 21:46:51 -0800

    add trending gifs and random gifs functionality

--- src/index.js
+++ src/index.js
@@ -8,11 +8,39 @@ function getGif(keyword) {
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
+    displayGif(response,keyword);
+  });
+
+  request.open("GET", url, true);
+  request.send();
+
+}
+
+function getTrending() {
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/trending?limit=5&api_key=${process.env.API_KEY}`;
+  
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
     if(this.status === 200) {
-      displayGif(response, keyword);
+      displayTrending(response);
     } else {
-      displayError(this, keyword);
+      displayError(this);
     }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+
+}
+
+function handleRandomSubmission() {
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}`;
+  
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    displayRandom(response);
 
   });
 
@@ -20,17 +48,26 @@ function getGif(keyword) {
   request.send();
 }
 
-
 // UI logic
 function displayGif(apiResponse, keyword) {
   document.getElementById("result").innerText = `Keyword: ${keyword}`;
-  document.getElementById("gifDisplay").setAttribute("src", apiResponse.data[0].images.fixed_height.url);
+  document.getElementById("gifDisplay").setAttribute("src", apiResponse.data[0].images.original.url);
 }
 
-function displayError(request, keyword) {
-  document.getElementById("result").innerText = `Error for keyword ${keyword}: ${request.status} ${(request.status === 401) ? "incorrect API Key": "incorrect URL"}`;
+function displayError(request) {
+  document.getElementById("result").innerText = `Error: ${request.status} ${(request.status === 401) ? "incorrect API Key": "incorrect URL"}`;
 }
 
+function displayTrending(apiResponse) {
+  for(let i = 1; i <= 5; i++) {
+    document.getElementById(`${i}`).setAttribute("src", apiResponse.data[i-1].images.original.url);
+  }
+
+}
+
+function displayRandom(apiResponse) {
+  document.getElementById("randomGifDisplay").setAttribute("src", apiResponse.data.images.original.url);
+}
 
 function handleSubmission(e) {
   e.preventDefault();
@@ -39,7 +76,8 @@ function handleSubmission(e) {
   getGif(input);
 }
 
-window.addEventListener("load", function() {
-  document.getElementById("search").addEventListener("submit", handleSubmission);
-});
+document.getElementById("search").addEventListener("submit", handleSubmission);
+getTrending();
+document.getElementById("random").addEventListener("click", handleRandomSubmission);
+
 
